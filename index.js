0cde5999e8296fdf278b6891fa37ba45556e4849
BryamFLA <bryam.assolini@docente.pr.senac.br>
2024-07-26 18:07:19 -0300

    att funcionalidade das fotos

--- index.js
+++ index.js
@@ -1,43 +1,62 @@
+// script.js
+
+// Firebase Configuração e Inicialização
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.9.0/firebase-app.js';
+import { getStorage, ref, listAll, getDownloadURL, getMetadata } from 'https://www.gstatic.com/firebasejs/9.9.0/firebase-storage.js';
+
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
+
 async function loadImages() {
     try {
-        const response = await fetch('https://fotos-back.vercel.app/images');
-        const images = await response.json();
-        if (Array.isArray(images)) {
-            const gallery = document.getElementById('gallery');
-            gallery.innerHTML = ''; // Limpar a galeria antes de adicionar novas imagens
+        const listRef = ref(storage, 'images/');
+        const res = await listAll(listRef);
 
-            images.forEach(image => {
-                const container = document.createElement('div'); // Contêiner para imagem e nome
-                container.className = 'image-container'; // Classe para estilização
+        const gallery = document.getElementById('gallery');
+        gallery.innerHTML = ''; // Limpar a galeria antes de adicionar novas imagens
 
-                let top = Math.floor(Math.random() * 100);
-                let left = Math.floor(Math.random() * 100);
-                let rotate = rotateGenerator();
-                while (rotate < -35 || rotate > 35) {
-                    rotate = rotateGenerator();
-                }
+        for (const itemRef of res.items) {
+            const url = await getDownloadURL(itemRef);
+            const metadata = await getMetadata(itemRef);
+            const names = metadata.customMetadata?.nomes || 'N/A';
+            const container = document.createElement('div'); // Contêiner para imagem e nome
+            container.className = 'image-container'; // Classe para estilização
 
-                container.style.top = `${top}vh`;
-                container.style.left = `${left}vw`;
-                container.style.rotate = `${rotate}deg`;
-                container.style.zIndex = '-1';
+            let top = Math.floor(Math.random() * 100);
+            let left = Math.floor(Math.random() * 100);
+            let rotate = rotateGenerator();
+            while (rotate < -35 || rotate > 35) {
+                rotate = rotateGenerator();
+            }
 
-                const img = document.createElement('img');
-                img.src = `data:${image.contentType};base64,${image.base64}`;
-                img.alt = image.name; // Adicionar texto alternativo
+            container.style.top = `${top}vh`;
+            container.style.left = `${left}vw`;
+            container.style.rotate = `${rotate}deg`;
+            container.style.zIndex = '-1';
 
-                const nameDiv = document.createElement('h4');
-                nameDiv.className = 'nomes'; // Classe para estilização
-                nameDiv.textContent = image.names; // Adiciona o nome da imagem
+            const img = document.createElement('img');
+            img.src = url;
+            img.alt = names; // Adicionar texto alternativo
 
-                container.appendChild(img);
-                container.appendChild(nameDiv);
-                gallery.appendChild(container);
-            });
+            const nameDiv = document.createElement('h4');
+            nameDiv.className = 'nomes'; // Classe para estilização
+            nameDiv.textContent = names; // Adiciona o nome da imagem
 
-        } else {
-            console.error('Erro: a resposta não é uma matriz:', images);
+            container.appendChild(img);
+            container.appendChild(nameDiv);
+            gallery.appendChild(container);
         }
+
     } catch (error) {
         console.error('Erro ao carregar imagens:', error);
     }
